97511b32bd7610b74021c1f194c129adbf2ba31d
paul mogesi <Paul.nyagah@strathmore.edu>
2017-10-25 23:22:18 +0300

    first commit

---
+++ app/src/main/java/paulnyagah/com/contact/Contact.java
@@ -0,0 +1,56 @@
+package paulnyagah.com.contact;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Contact {
+
+    int _id;
+    String _name;
+    String _phone_number;
+
+    public Contact(){
+
+    }
+
+    public Contact(int id, String name, String _phone_number){
+        this._id = id;
+        this._name = name;
+        this._phone_number = _phone_number;
+    }
+
+    public Contact(String name, String _phone_number){
+        this._name = name;
+        this._phone_number = _phone_number;
+    }
+
+    public int getID(){
+        return this._id;
+    }
+
+    public void setID(int id){
+        this._id = id;
+    }
+
+    public String getName(){
+        return this._name;
+    }
+
+    public void setName(String name){
+        this._name = name;
+    }
+
+    public String getPhoneNumber(){
+        return this._phone_number;
+    }
+
+    public void setPhoneNumber(String phone_number){
+        this._phone_number = phone_number;
+    }
+}
